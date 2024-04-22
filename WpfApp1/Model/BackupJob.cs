1a1ab18aab0207dbd0190e6b953597b21f176ea2
theazyfa <oullion.m@gmail.com>
2024-04-22 23:50:37 +0200

    Add files via upload

---
+++ WpfApp1/Model/BackupJob.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1.Model
+{
+    public class BackupJob:INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        public int Progress { get; set; } = 0;
+
+     
+
+        internal void Start()
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerAsync();
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            for (int i = 0; i < 100; i++)
+            {
+                System.Threading.Thread.Sleep(RandomNumberGenerator.GetInt32(1000,2000));
+                Progress++;
+                NotifyPropertyChanged("Progress");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ WpfApp1/ViewModel/MainWindowsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public class MainWindowsViewModel
+    {
+
+        //le type ObservableCollection permet de recevoir des événements en cas d'ajout ou de retrait
+        public ObservableCollection<BackupJob> Jobs { get; set;  } = new ObservableCollection<BackupJob>();
+
+        public void AddBackupJob()
+        {
+            BackupJob job = new BackupJob();
+            job.Name = "Jobs";
+            job.Progress = 0;
+            job.Start();
+            Jobs.Add(job);
+        }
+
+    }
+}
